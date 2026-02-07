5455b25bc59be6f9f8e0acfd3b38ae6b29d76a3d
chandu078 <chandudyavanapelli03@gmail.com>
2026-02-07 05:13:49 +0000

    macan: Switch to prebuilt kernel
    Change-Id: I7cc8459071da010f873fd49122dcab020d9eb4ba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+USE_PREBUILT_KERNEL ?= true
+
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 16231956480
 
@@ -19,7 +21,11 @@ TARGET_OTA_ASSERT_DEVICE := OP612DL1
 TARGET_SCREEN_DENSITY := 560
 
 # Kernel
+ifeq ($(USE_PREBUILT_KERNEL), true)
+include device/oneplus/macan-kernel/BoardConfig.mk
+else
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_MACAN_DTB=y
+endif
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
